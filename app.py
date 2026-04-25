8e4aca8046194e92c82198cba93bd58421b58e66
Hassaan <hassaansiddiqui71@gmail.com>
2026-04-25 22:05:33 +0500

    added CI/CD pipeline

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-app = Flask (__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello  my devops app is live  "
+    return "HelloDevops"
 
-if __name__ == '__main__ ':
-   app.run (host='0.0.0.0', port=5000)
-
+if __name__ == "__main__" :
+    app.run(host="0.0.0.0", port=5000)
